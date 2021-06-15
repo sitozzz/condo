b0aa30f826f36cef01530a39d3a8d725c42ff942
Dmitry Danev <danewdima@gmail.com>
2021-06-15 07:46:45 +0300

    SBERDOMA-372 add reinvite organization employee mutation

--- apps/condo/domains/organization/schema/InviteNewOrganizationEmployeeService.js
+++ apps/condo/domains/organization/schema/InviteNewOrganizationEmployeeService.js
@@ -1,13 +1,17 @@
 const passwordGenerator = require('generate-password')
-const { getById, GQLCustomSchema } = require('@core/keystone/schema')
-
+const { GQLCustomSchema } = require('@core/keystone/schema')
+const { REGISTER_NEW_USER_MUTATION } = require('@condo/domains/user/gql')
+const { normalizePhone } = require('@condo/domains/common/utils/phone')
+const { sendMessage } = require('@condo/domains/notification/utils/serverSchema')
+const { PHONE_WRONG_FORMAT_ERROR } = require('@condo/domains/common/constants/errors')
+const { Organization, OrganizationEmployee } = require('@condo/domains/organization/utils/serverSchema')
+const { INVITE_NEW_EMPLOYEE_MESSAGE_TYPE } = require('@condo/domains/notification/constants')
 const { createOrganizationEmployee } = require('../../../utils/serverSchema/Organization')
 const { rules } = require('../../../access')
 const guards = require('../utils/serverSchema/guards')
-const {findOrganizationEmployee} = require("../../../utils/serverSchema/Organization");
-const { normalizePhone } = require('@condo/domains/common/utils/phone')
-const { PHONE_WRONG_FORMAT_ERROR } = require('@condo/domains/common/constants/errors')
-const { REGISTER_NEW_USER_MUTATION } = require('../../user/gql')
+const { ALREADY_EXISTS_ERROR, NOT_FOUND_ERROR } = require('@condo/domains/common/constants/errors')
+const get = require('lodash/get')
+const { getById } = require('@core/keystone/schema')
 
 const InviteNewOrganizationEmployeeService = new GQLCustomSchema('InviteNewOrganizationEmployeeService', {
     types: [
@@ -17,7 +21,7 @@ const InviteNewOrganizationEmployeeService = new GQLCustomSchema('InviteNewOrgan
         },
         {
             access: true,
-            type: 'input ReInviteOrganizationEmployeeInput { organization: OrganizationWhereUniqueInput!, employee: EmployeeWhereInput!}',
+            type: 'input ReInviteOrganizationEmployeeInput { dv: Int!, sender: JSON!, organization: OrganizationWhereUniqueInput!, email: String!, phone: String}',
         },
     ],
     mutations: [
@@ -34,7 +38,12 @@ const InviteNewOrganizationEmployeeService = new GQLCustomSchema('InviteNewOrgan
                 // TODO(pahaz): normalize email!
 
                 let user = await guards.checkUserExistency(context, email, phone)
-                await guards.checkEmployeeExistency(context, organization, email, phone, user)
+                const existedEmployee = await guards.checkEmployeeExistency(context, organization, email, phone, user)
+
+                if (existedEmployee) {
+                    const msg = `${ALREADY_EXISTS_ERROR}employee unique] User is already invited in the organization`
+                    throw new Error(msg)
+                }
 
                 if (!user) {
                     const password = passwordGenerator.generate({
@@ -76,40 +85,84 @@ const InviteNewOrganizationEmployeeService = new GQLCustomSchema('InviteNewOrgan
                     ...restData,
                 })
 
+                const organizationCountry = get(organization, 'country', 'en')
+                const organizationName = get(organization, 'name')
+
+                await sendMessage(context, {
+                    lang: organizationCountry,
+                    to: {
+                        user: {
+                            id: user.id,
+                        },
+                    },
+                    type: INVITE_NEW_EMPLOYEE_MESSAGE_TYPE,
+                    meta: {
+                        organizationName,
+                        dv: 1,
+                    },
+                    sender: data.sender,
+                })
+
                 return await getById('OrganizationEmployee', employee.id)
             },
         },
         {
             access: rules.canInviteEmployee,
-            schema: 'reInviteNewOrganizationEmployee(data: ReInviteOrganizationEmployeeInput!): OrganizationEmployee',
+            schema: 'reInviteOrganizationEmployee(data: ReInviteOrganizationEmployeeInput!): OrganizationEmployee',
             resolver: async (parent, args, context) => {
-                if (!context.authedItem.id) throw new Error('[error] User is not authenticated')
+                if (!context.authedItem.id) {
+                    throw new Error('[error] User is not authenticated')
+                }
                 const { data } = args
-                let { organization, employee } = data
-                /*
-                *   1) Проверить есть ли empoloyee в организации и совпадает ли организация того кто триггерит реинвайт с организацией того кого реинвайтим
-                *   2) Найти сообщения по типу  ID пользователя.
-                *   3) Стригеррить реинвайт сообщения
-                * */
+                const { organization, email, sender } = data
+                let { phone } = data
+                phone = normalizePhone(phone)
+                if (!phone) {
+                    throw new Error(`${PHONE_WRONG_FORMAT_ERROR}phone] invalid format`)
+                }
 
-                const employeeById = await findOrganizationEmployee(context, {
-                    email,
-                    organization: { id: organization.id },
-                    deletedAt: null,
+                const [employeeOrganization] = await Organization.getAll(context, { id: organization.id })
+
+                if (!employeeOrganization) {
+                    throw new Error('No organization found for OrganizationEmployeeRole')
+                }
+
+                const existedUser = await guards.checkUserExistency(context, email, phone)
+                if (!existedUser) {
+                    const msg = `${NOT_FOUND_ERROR}user undef] There is no user for employee`
+                    throw new Error(msg)
+                }
+
+                const existedEmployee = await guards.checkEmployeeExistency(context, organization, email, phone, existedUser)
+                if (!existedEmployee) {
+                    const msg = `${NOT_FOUND_ERROR}employee undef] There is no employee found invited to organization`
+                    throw new Error(msg)
+                }
+
+                if (get(existedEmployee, 'isAccepted')) {
+                    const msg = `${ALREADY_EXISTS_ERROR}employee unique] User is already accepted organization invitation`
+                    throw new Error(msg)
+                }
+
+                const organizationCountry = get(employeeOrganization, 'country', 'en')
+                const organizationName = get(employeeOrganization, 'name')
+
+                await sendMessage(context, {
+                    lang: organizationCountry,
+                    to: {
+                        user: {
+                            id: existedUser.id,
+                        },
+                    },
+                    type: INVITE_NEW_EMPLOYEE_MESSAGE_TYPE,
+                    meta: {
+                        organizationName,
+                        dv: 1,
+                    },
+                    sender: sender,
                 })
 
-                // const employee = await createOrganizationEmployee(context, {
-                //     user: { connect: { id: user.id } },
-                //     organization: { connect: { id: organization.id } },
-                //     ...role && { role: { connect: { id: role.id } } },
-                //     position,
-                //     email,
-                //     name,
-                //     phone,
-                //     ...restData,
-                // })
-
-                return await getById('OrganizationEmployee', employee.id)
+                return await getById('OrganizationEmployee', existedEmployee.id)
             },
         },
     ],
