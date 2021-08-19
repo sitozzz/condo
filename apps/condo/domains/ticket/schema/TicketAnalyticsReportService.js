f64f82137d502d953597725ff92a962613f959b2
Sitnikov Ivan <sit7602@gmail.com>
2021-08-19 14:41:04 +0500

    SBERDOMA-1030 - add custom query for exporting analytics report

--- apps/condo/domains/ticket/schema/TicketAnalyticsReportService.js
+++ apps/condo/domains/ticket/schema/TicketAnalyticsReportService.js
@@ -7,18 +7,20 @@ const access = require('@condo/domains/ticket/access/TicketAnalyticsReportServic
 const moment = require('moment')
 const { sortStatusesByType } = require('@condo/domains/ticket/utils/serverSchema/analytics.helper')
 const { TicketAnalyticsQueryBuilder } = require('@condo/domains/ticket/utils/serverSchema/analytics.helper')
-const { DATE_DISPLAY_FORMAT } = require('@condo/domains/ticket/constants/common')
+const { DATE_DISPLAY_FORMAT, TICKET_REPORT_DAY_GROUP_STEPS } = require('@condo/domains/ticket/constants/common')
 const { Property: PropertyServerUtils } = require('@condo/domains/property/utils/serverSchema')
-const { TicketStatus: TicketStatusServerUtils } = require('@condo/domains/ticket/utils/serverSchema')
+const { TicketStatus: TicketStatusServerUtils, Ticket } = require('@condo/domains/ticket/utils/serverSchema')
 const isEmpty = require('lodash/isEmpty')
 const get = require('lodash/get')
+const groupObjectBy = require('lodash/groupBy')
+const { createExportFile } = require('@condo/domains/common/utils/createExportFile')
 
 const createPropertyRange = async (context, organizationWhereInput) => {
     const properties = await PropertyServerUtils.getAll(context, { organization:  organizationWhereInput  })
     return properties.map( property => ({ label: property.address, value: property.id }))
 }
 
-const createStatusRange = async (context, organizationWhereInput) => {
+const createStatusRange = async (context, organizationWhereInput, labelKey = 'name') => {
     const statuses = await TicketStatusServerUtils.getAll(context, { OR: [
         { organization: organizationWhereInput },
         { organization_is_null: true },
@@ -32,7 +34,85 @@ const createStatusRange = async (context, organizationWhereInput) => {
         return !statuses
             .find(organizationStatus => organizationStatus.organization !== null && organizationStatus.type === status.type)
     })
-    return sortStatusesByType(allStatuses).map(status => ({ label: status.name, value: status.id }))
+    return sortStatusesByType(allStatuses).map(status => ({ label: status[labelKey], value: status.id }))
+}
+
+const getTicketCounts = async (context, where, groupBy, extraLabels = {}) => {
+    const analyticsQueryBuilder = new TicketAnalyticsQueryBuilder(where, groupBy)
+    await analyticsQueryBuilder.loadData()
+
+    const translates = {}
+    for (const group of groupBy) {
+        switch (group) {
+            case 'property':
+                translates[group] = await createPropertyRange(context, where.organization)
+                break
+            case 'status':
+                translates[group] = await createStatusRange(
+                    context, where.organization, isEmpty(extraLabels) ? 'name' :  extraLabels[group]
+                )
+                break
+            default:
+                break
+        }
+    }
+
+    return analyticsQueryBuilder
+        .getResult(({ count, dayGroup, ...searchResult }) =>
+        {
+            if (!isEmpty(translates)) {
+                Object.entries(searchResult).forEach(([groupName, value]) => {
+                    const translateMapping = get(translates, groupName, false)
+                    if (translateMapping) {
+                        const translation = translateMapping.find(translate => translate.value === value)
+                        searchResult[groupName] = translation.label
+                    }
+                })
+                return {
+                    ...searchResult,
+                    dayGroup: moment(dayGroup).format(DATE_DISPLAY_FORMAT),
+                    count: parseInt(count),
+                }
+            }
+            return {
+                ...searchResult,
+                dayGroup: moment(dayGroup).format(DATE_DISPLAY_FORMAT),
+                count:parseInt(count),
+            }
+        })
+}
+
+const aggregateData = (data, groupByFilter) => {
+    const [axisGroupKey] = groupByFilter
+    const labelsGroupKey = TICKET_REPORT_DAY_GROUP_STEPS.includes(groupByFilter[1]) ? 'dayGroup' : groupByFilter[1]
+    const groupedResult = groupObjectBy(data, axisGroupKey)
+    const result = {}
+    Object.entries(groupedResult).forEach(([filter, dataObject]) => {
+        result[filter] = Object.fromEntries(
+            Object.entries(
+                groupObjectBy(dataObject, labelsGroupKey)
+            ).map(([labelsGroupTitle, resultObject]) => [labelsGroupTitle, resultObject[0].count])
+        )
+    })
+    return { result, groupKeys: [axisGroupKey, labelsGroupKey] }
+}
+
+const ticketAnalyticsExcelExportDataMapper = (data) => {
+    const uniqueDates = Array.from(new Set(Object.values(data).flatMap(e => Object.keys(e))))
+    const result = []
+    uniqueDates.forEach((date, key) => {
+        const restTableColumns = {}
+        Object.keys(data).forEach(ticketType => {
+            restTableColumns[ticketType] = data[ticketType][date]
+        })
+        let address = 'Все адреса'
+        // const addressList = get(filters, 'addresses')
+        // if (addressList && addressList.length) {
+        //     address = addressList.join(', ')
+        // }
+        result.push({ key, address, date, ...restTableColumns })
+    })
+    return result
 }
 
 const TicketAnalyticsReportService = new GQLCustomSchema('TicketAnalyticsReportService', {
@@ -51,7 +131,11 @@ const TicketAnalyticsReportService = new GQLCustomSchema('TicketAnalyticsReportS
         },
         {
             access: true,
-            type: 'type TicketGroupedCounter { count: Int!, status: String, property: String, dayGroup: String!  }',
+            type: 'type TicketGroupedCounter { count: Int!, status: String, property: String, dayGroup: String! }',
+        },
+        {
+            access: true,
+            type: 'type ExportTicketAnalyticsToExcelOutput { link: String! }',
         },
     ],
     queries: [
@@ -60,51 +144,33 @@ const TicketAnalyticsReportService = new GQLCustomSchema('TicketAnalyticsReportS
             schema: 'ticketAnalyticsReport(data: TicketAnalyticsReportInput): TicketAnalyticsReportOutput',
             resolver: async (parent, args, context, info, extra = {}) => {
                 const { data: { where = {}, groupBy = [] } } = args
-
-                const analyticsQueryBuilder = new TicketAnalyticsQueryBuilder(where, groupBy)
-                await analyticsQueryBuilder.loadData()
-
-                const translates = {}
-                for (const group of groupBy) {
-                    switch (group) {
-                        case 'property':
-                            translates[group] = await createPropertyRange(context, where.organization)
-                            break
-                        case 'status':
-                            translates[group] = await createStatusRange(context, where.organization)
-                            break
-                        default:
-                            break
-                    }
-                }
-
-                const result = analyticsQueryBuilder
-                    .getResult(({ count, dayGroup, ...searchResult }) =>
-                    {
-                        if (!isEmpty(translates)) {
-                            Object.entries(searchResult).forEach(([groupName, value]) => {
-                                const translateMapping = get(translates, groupName, false)
-                                if (translateMapping) {
-                                    searchResult[groupName] = translateMapping.find(translate => translate.value === value).label
-                                }
-                            })
-                            return {
-                                ...searchResult,
-                                dayGroup: moment(dayGroup).format(DATE_DISPLAY_FORMAT),
-                                count: parseInt(count),
-                            }
-                        }
-                        return {
-                            ...searchResult,
-                            dayGroup: moment(dayGroup).format(DATE_DISPLAY_FORMAT),
-                            count:parseInt(count),
-                        }
-                    })
+                const result = await getTicketCounts(context, where, groupBy)
                 return { result }
             },
         },
+        {
+            access: access.canReadTicketAnalyticsReport,
+            schema: 'exportTicketAnalyticsToExcel(data: TicketAnalyticsReportInput): ExportTicketAnalyticsToExcelOutput',
+            resolver: async (parent, args, context, info, extra = {}) => {
+                const { data: { where = {}, groupBy = [] } } = args
+                const ticketCounts = await getTicketCounts(context, where, groupBy, { status: 'type' })
+                const { result, groupKeys } = aggregateData(ticketCounts, groupBy)
+                const ticketAccessCheck = await Ticket.getAll(context, where, { first: 1 })
+                const [groupBy1, groupBy2] = groupKeys
+                const excelRows = ticketAnalyticsExcelExportDataMapper(result)
+                const link = await createExportFile({
+                    fileName: `ticket_analytics_${moment().format('DD_MM')}.xlsx`,
+                    templatePath: `./domains/ticket/templates/TicketAnalyticsExportTemplate[${groupBy1}_${groupBy2}].xlsx`,
+                    replaces: { tickets: excelRows },
+                    meta: {
+                        listkey: 'Ticket',
+                        id: ticketAccessCheck[0].id,
+                    },
+                })
+                return { link }
+            },
+        },
     ],
-
 })
 
 module.exports = {
--- apps/condo/schema.d.ts
+++ apps/condo/schema.d.ts
@@ -5262,6 +5262,12 @@ export type Division = {
   organization?: Maybe<Organization>;
   /**  Person, responsible for this division  */
   responsible?: Maybe<OrganizationEmployee>;
+  /**  Properties in service by this division  */
+  properties: Array<Property>;
+  _propertiesMeta?: Maybe<_QueryMeta>;
+  /**  Employees, that will be assigned as executors to all corresponding tickets  */
+  executors: Array<OrganizationEmployee>;
+  _executorsMeta?: Maybe<_QueryMeta>;
   id: Scalars['ID'];
   v?: Maybe<Scalars['Int']>;
   createdAt?: Maybe<Scalars['String']>;
@@ -5272,12 +5278,58 @@ export type Division = {
   newId?: Maybe<Scalars['String']>;
 };
 
+
+/**  Grouping of properties and employees with one single responsible person  */
+export type DivisionPropertiesArgs = {
+  where?: Maybe<PropertyWhereInput>;
+  search?: Maybe<Scalars['String']>;
+  sortBy?: Maybe<Array<SortPropertiesBy>>;
+  orderBy?: Maybe<Scalars['String']>;
+  first?: Maybe<Scalars['Int']>;
+  skip?: Maybe<Scalars['Int']>;
+};
+
+
+/**  Grouping of properties and employees with one single responsible person  */
+export type Division_PropertiesMetaArgs = {
+  where?: Maybe<PropertyWhereInput>;
+  search?: Maybe<Scalars['String']>;
+  sortBy?: Maybe<Array<SortPropertiesBy>>;
+  orderBy?: Maybe<Scalars['String']>;
+  first?: Maybe<Scalars['Int']>;
+  skip?: Maybe<Scalars['Int']>;
+};
+
+
+/**  Grouping of properties and employees with one single responsible person  */
+export type DivisionExecutorsArgs = {
+  where?: Maybe<OrganizationEmployeeWhereInput>;
+  search?: Maybe<Scalars['String']>;
+  sortBy?: Maybe<Array<SortOrganizationEmployeesBy>>;
+  orderBy?: Maybe<Scalars['String']>;
+  first?: Maybe<Scalars['Int']>;
+  skip?: Maybe<Scalars['Int']>;
+};
+
+
+/**  Grouping of properties and employees with one single responsible person  */
+export type Division_ExecutorsMetaArgs = {
+  where?: Maybe<OrganizationEmployeeWhereInput>;
+  search?: Maybe<Scalars['String']>;
+  sortBy?: Maybe<Array<SortOrganizationEmployeesBy>>;
+  orderBy?: Maybe<Scalars['String']>;
+  first?: Maybe<Scalars['Int']>;
+  skip?: Maybe<Scalars['Int']>;
+};
+
 export type DivisionCreateInput = {
   dv?: Maybe<Scalars['Int']>;
   sender?: Maybe<Scalars['JSON']>;
   name?: Maybe<Scalars['String']>;
   organization?: Maybe<OrganizationRelateToOneInput>;
   responsible?: Maybe<OrganizationEmployeeRelateToOneInput>;
+  properties?: Maybe<PropertyRelateToManyInput>;
+  executors?: Maybe<OrganizationEmployeeRelateToManyInput>;
   v?: Maybe<Scalars['Int']>;
   createdAt?: Maybe<Scalars['String']>;
   updatedAt?: Maybe<Scalars['String']>;
@@ -5484,6 +5536,8 @@ export type DivisionUpdateInput = {
   name?: Maybe<Scalars['String']>;
   organization?: Maybe<OrganizationRelateToOneInput>;
   responsible?: Maybe<OrganizationEmployeeRelateToOneInput>;
+  properties?: Maybe<PropertyRelateToManyInput>;
+  executors?: Maybe<OrganizationEmployeeRelateToManyInput>;
   v?: Maybe<Scalars['Int']>;
   createdAt?: Maybe<Scalars['String']>;
   updatedAt?: Maybe<Scalars['String']>;
@@ -5530,6 +5584,18 @@ export type DivisionWhereInput = {
   organization_is_null?: Maybe<Scalars['Boolean']>;
   responsible?: Maybe<OrganizationEmployeeWhereInput>;
   responsible_is_null?: Maybe<Scalars['Boolean']>;
+  /**  condition must be true for all nodes  */
+  properties_every?: Maybe<PropertyWhereInput>;
+  /**  condition must be true for at least 1 node  */
+  properties_some?: Maybe<PropertyWhereInput>;
+  /**  condition must be false for all nodes  */
+  properties_none?: Maybe<PropertyWhereInput>;
+  /**  condition must be true for all nodes  */
+  executors_every?: Maybe<OrganizationEmployeeWhereInput>;
+  /**  condition must be true for at least 1 node  */
+  executors_some?: Maybe<OrganizationEmployeeWhereInput>;
+  /**  condition must be false for all nodes  */
+  executors_none?: Maybe<OrganizationEmployeeWhereInput>;
   id?: Maybe<Scalars['ID']>;
   id_not?: Maybe<Scalars['ID']>;
   id_in?: Maybe<Array<Maybe<Scalars['ID']>>>;
@@ -5587,6 +5653,11 @@ export type DivisionsCreateInput = {
 export type DivisionsUpdateInput = {
   id: Scalars['ID'];
   data?: Maybe<DivisionUpdateInput>;
+};
+
+export type ExportTicketAnalyticsToExcelOutput = {
+  __typename?: 'ExportTicketAnalyticsToExcelOutput';
+  link: Scalars['String'];
 };
 
 export type ExportTicketsToExcelInput = {
@@ -9706,7 +9777,6 @@ export type OrganizationEmployee = {
    *  4. As an alias to the 'id' field on the OrganizationEmployee List.
    */
   _label_?: Maybe<Scalars['String']>;
-  id: Scalars['ID'];
   /**  Data structure Version  */
   dv?: Maybe<Scalars['Int']>;
   /**  Client-side devise identification used for the anti-fraud detection. Example `{ dv: '1', fingerprint: 'VaxSw2aXZa'}`. Where the `fingerprint` should be the same for the same devices and it's not linked to the user ID. It's the device ID like browser / mobile application / remote system  */
@@ -9730,6 +9800,7 @@ export type OrganizationEmployee = {
   isRejected?: Maybe<Scalars['Boolean']>;
   /**  Employee is blocked status, used in permissions functions, isBlocked has Free-form description of the employee's position over all permissions  */
   isBlocked?: Maybe<Scalars['Boolean']>;
+  id: Scalars['ID'];
   v?: Maybe<Scalars['Int']>;
   createdAt?: Maybe<Scalars['String']>;
   updatedAt?: Maybe<Scalars['String']>;
@@ -9809,12 +9880,12 @@ export type OrganizationEmployeeHistoryRecord = {
   isAccepted?: Maybe<Scalars['Boolean']>;
   isRejected?: Maybe<Scalars['Boolean']>;
   isBlocked?: Maybe<Scalars['Boolean']>;
+  id: Scalars['ID'];
   v?: Maybe<Scalars['Int']>;
   createdAt?: Maybe<Scalars['String']>;
   updatedAt?: Maybe<Scalars['String']>;
   createdBy?: Maybe<Scalars['String']>;
   updatedBy?: Maybe<Scalars['String']>;
-  id: Scalars['ID'];
   history_date?: Maybe<Scalars['String']>;
   history_action?: Maybe<OrganizationEmployeeHistoryRecordHistoryActionType>;
   history_id?: Maybe<Scalars['String']>;
@@ -9983,6 +10054,10 @@ export type OrganizationEmployeeHistoryRecordWhereInput = {
   isRejected_not?: Maybe<Scalars['Boolean']>;
   isBlocked?: Maybe<Scalars['Boolean']>;
   isBlocked_not?: Maybe<Scalars['Boolean']>;
+  id?: Maybe<Scalars['ID']>;
+  id_not?: Maybe<Scalars['ID']>;
+  id_in?: Maybe<Array<Maybe<Scalars['ID']>>>;
+  id_not_in?: Maybe<Array<Maybe<Scalars['ID']>>>;
   v?: Maybe<Scalars['Int']>;
   v_not?: Maybe<Scalars['Int']>;
   v_lt?: Maybe<Scalars['Int']>;
@@ -10015,10 +10090,6 @@ export type OrganizationEmployeeHistoryRecordWhereInput = {
   updatedBy_not?: Maybe<Scalars['String']>;
   updatedBy_in?: Maybe<Array<Maybe<Scalars['String']>>>;
   updatedBy_not_in?: Maybe<Array<Maybe<Scalars['String']>>>;
-  id?: Maybe<Scalars['ID']>;
-  id_not?: Maybe<Scalars['ID']>;
-  id_in?: Maybe<Array<Maybe<Scalars['ID']>>>;
-  id_not_in?: Maybe<Array<Maybe<Scalars['ID']>>>;
   history_date?: Maybe<Scalars['String']>;
   history_date_not?: Maybe<Scalars['String']>;
   history_date_lt?: Maybe<Scalars['String']>;
@@ -10567,14 +10638,6 @@ export type OrganizationEmployeeUpdateInput = {
 export type OrganizationEmployeeWhereInput = {
   AND?: Maybe<Array<Maybe<OrganizationEmployeeWhereInput>>>;
   OR?: Maybe<Array<Maybe<OrganizationEmployeeWhereInput>>>;
-  id?: Maybe<Scalars['ID']>;
-  id_not?: Maybe<Scalars['ID']>;
-  id_lt?: Maybe<Scalars['ID']>;
-  id_lte?: Maybe<Scalars['ID']>;
-  id_gt?: Maybe<Scalars['ID']>;
-  id_gte?: Maybe<Scalars['ID']>;
-  id_in?: Maybe<Array<Maybe<Scalars['ID']>>>;
-  id_not_in?: Maybe<Array<Maybe<Scalars['ID']>>>;
   dv?: Maybe<Scalars['Int']>;
   dv_not?: Maybe<Scalars['Int']>;
   dv_lt?: Maybe<Scalars['Int']>;
@@ -10681,6 +10744,10 @@ export type OrganizationEmployeeWhereInput = {
   isRejected_not?: Maybe<Scalars['Boolean']>;
   isBlocked?: Maybe<Scalars['Boolean']>;
   isBlocked_not?: Maybe<Scalars['Boolean']>;
+  id?: Maybe<Scalars['ID']>;
+  id_not?: Maybe<Scalars['ID']>;
+  id_in?: Maybe<Array<Maybe<Scalars['ID']>>>;
+  id_not_in?: Maybe<Array<Maybe<Scalars['ID']>>>;
   v?: Maybe<Scalars['Int']>;
   v_not?: Maybe<Scalars['Int']>;
   v_lt?: Maybe<Scalars['Int']>;
@@ -11777,6 +11844,13 @@ export type PropertyHistoryRecordsUpdateInput = {
   data?: Maybe<PropertyHistoryRecordUpdateInput>;
 };
 
+export type PropertyRelateToManyInput = {
+  create?: Maybe<Array<Maybe<PropertyCreateInput>>>;
+  connect?: Maybe<Array<Maybe<PropertyWhereUniqueInput>>>;
+  disconnect?: Maybe<Array<Maybe<PropertyWhereUniqueInput>>>;
+  disconnectAll?: Maybe<Scalars['Boolean']>;
+};
+
 export type PropertyRelateToOneInput = {
   create?: Maybe<PropertyCreateInput>;
   connect?: Maybe<PropertyWhereUniqueInput>;
@@ -12470,6 +12544,7 @@ export type Query = {
   ticketReportWidgetData?: Maybe<TicketReportWidgetOutput>;
   exportTicketsToExcel?: Maybe<ExportTicketsToExcelOutput>;
   ticketAnalyticsReport?: Maybe<TicketAnalyticsReportOutput>;
+  exportTicketAnalyticsToExcel?: Maybe<ExportTicketAnalyticsToExcelOutput>;
   allResidentTickets?: Maybe<Array<Maybe<ResidentTicketOutput>>>;
   /** The version of the Keystone application serving this API. */
   appVersion?: Maybe<Scalars['String']>;
@@ -14166,6 +14241,11 @@ export type QueryExportTicketsToExcelArgs = {
 
 
 export type QueryTicketAnalyticsReportArgs = {
+  data?: Maybe<TicketAnalyticsReportInput>;
+};
+
+
+export type QueryExportTicketAnalyticsToExcelArgs = {
   data?: Maybe<TicketAnalyticsReportInput>;
 };
 
@@ -15990,6 +16070,10 @@ export enum SortDivisionsBy {
   OrganizationDesc = 'organization_DESC',
   ResponsibleAsc = 'responsible_ASC',
   ResponsibleDesc = 'responsible_DESC',
+  PropertiesAsc = 'properties_ASC',
+  PropertiesDesc = 'properties_DESC',
+  ExecutorsAsc = 'executors_ASC',
+  ExecutorsDesc = 'executors_DESC',
   IdAsc = 'id_ASC',
   IdDesc = 'id_DESC',
   VAsc = 'v_ASC',
@@ -16147,14 +16231,14 @@ export enum SortOrganizationEmployeeHistoryRecordsBy {
   IsRejectedDesc = 'isRejected_DESC',
   IsBlockedAsc = 'isBlocked_ASC',
   IsBlockedDesc = 'isBlocked_DESC',
+  IdAsc = 'id_ASC',
+  IdDesc = 'id_DESC',
   VAsc = 'v_ASC',
   VDesc = 'v_DESC',
   CreatedAtAsc = 'createdAt_ASC',
   CreatedAtDesc = 'createdAt_DESC',
   UpdatedAtAsc = 'updatedAt_ASC',
   UpdatedAtDesc = 'updatedAt_DESC',
-  IdAsc = 'id_ASC',
-  IdDesc = 'id_DESC',
   HistoryDateAsc = 'history_date_ASC',
   HistoryDateDesc = 'history_date_DESC',
   HistoryActionAsc = 'history_action_ASC',
@@ -16254,8 +16338,6 @@ export enum SortOrganizationEmployeeRolesBy {
 }
 
 export enum SortOrganizationEmployeesBy {
-  IdAsc = 'id_ASC',
-  IdDesc = 'id_DESC',
   DvAsc = 'dv_ASC',
   DvDesc = 'dv_DESC',
   OrganizationAsc = 'organization_ASC',
@@ -16282,6 +16364,8 @@ export enum SortOrganizationEmployeesBy {
   IsRejectedDesc = 'isRejected_DESC',
   IsBlockedAsc = 'isBlocked_ASC',
   IsBlockedDesc = 'isBlocked_DESC',
+  IdAsc = 'id_ASC',
+  IdDesc = 'id_DESC',
   VAsc = 'v_ASC',
   VDesc = 'v_DESC',
   CreatedAtAsc = 'createdAt_ASC',
