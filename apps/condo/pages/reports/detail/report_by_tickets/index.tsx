a8a154d1a9e82824ac6f2c2f154170c5cc6ba1e1
Sitnikov Ivan <sit7602@gmail.com>
2021-08-20 13:30:28 +0500

    SBERDOMA-1030 - move mapper method to helper & add property group support

--- apps/condo/pages/reports/detail/report_by_tickets/index.tsx
+++ apps/condo/pages/reports/detail/report_by_tickets/index.tsx
@@ -436,7 +436,9 @@ const TicketAnalyticsPage: ITicketAnalyticsPage = () => {
             const { AND, groupBy } = filterToQuery(filtersRef.current, viewMode, ticketType)
             const where = { organization: { id: userOrganizationId }, AND }
             const translates = {
-                property: filtersRef.current.addressList.length ? filtersRef.current.addressList.map(({ value }) => value).join(', ') : AllAddressTitle,
+                property: filtersRef.current.addressList.length ?
+                    filtersRef.current.addressList.map(({ value }) => value).join('@')
+                    : AllAddressTitle,
             }
 
             exportTicketAnalyticsToExcel({ variables: { data: { groupBy, where, translates } } })
@@ -452,6 +454,7 @@ const TicketAnalyticsPage: ITicketAnalyticsPage = () => {
     if (selectedAddresses.length > 1) {
         addressFilterTitle = ManyAddresses
     }
+    const isControlsDisabled = loading || isXSLXLoading || filtersRef.current === null
     return <>
         <Head>
             <title>{PageTitle}</title>
@@ -547,10 +550,10 @@ const TicketAnalyticsPage: ITicketAnalyticsPage = () => {
                         ), [analyticsData, loading, viewMode])}
                     </Col>
                     <ActionBar fullscreen>
-                        <Button onClick={printPdf} icon={<FilePdfFilled />} type='sberPrimary' secondary>
+                        <Button disabled={isControlsDisabled} onClick={printPdf} icon={<FilePdfFilled />} type='sberPrimary' secondary>
                             {PrintTitle}
                         </Button>
-                        <Button onClick={downloadExcel} loading={isXSLXLoading} icon={<EditFilled />} type='sberPrimary' secondary>{ExcelTitle}</Button>
+                        <Button disabled={isControlsDisabled} onClick={downloadExcel} loading={isXSLXLoading} icon={<EditFilled />} type='sberPrimary' secondary>{ExcelTitle}</Button>
                     </ActionBar>
                 </Row>
             </PageContent>
